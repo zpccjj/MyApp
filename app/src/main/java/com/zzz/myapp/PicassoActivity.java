4511811c4a5a1d282f5463bd8eb064dbee4731f8
朱盼超 <zpc911@126.com>
2019-03-06 09:48:57 +0800

    Add Picasso Adapter

--- app/src/main/java/com/zzz/myapp/PicassoActivity.java
+++ app/src/main/java/com/zzz/myapp/PicassoActivity.java
@@ -3,12 +3,11 @@ package com.zzz.myapp;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 import com.qmuiteam.qmui.widget.QMUITopBar;
-import com.squareup.picasso.MemoryPolicy;
-import com.squareup.picasso.Picasso;
+import com.zzz.myapp.adpater.PicassoListAdapter;
 import com.zzz.myapp.ui.HttpActivity;
 
 import butterknife.BindView;
@@ -16,7 +15,24 @@ import butterknife.ButterKnife;
 
 public class PicassoActivity extends HttpActivity {
     @BindView(R.id.topbar) QMUITopBar mTopBar;
-    @BindView(R.id.picasso_imageview) ImageView mImageView;
+    @BindView(R.id.picasso_listview) ListView mListView;
+
+    public static String[] imageUrls = {
+            "http://i.imgur.com/rFLNqWI.jpg",
+            "http://i.imgur.com/C9pBVt7.jpg",
+            "http://i.imgur.com/rT5vXE1.jpg",
+            "http://i.imgur.com/aIy5R2k.jpg",
+            "http://i.imgur.com/MoJs9pT.jpg",
+            "http://i.imgur.com/S963yEM.jpg",
+            "http://i.imgur.com/rLR2cyc.jpg",
+            "http://i.imgur.com/SEPdUIx.jpg",
+            "http://i.imgur.com/aC9OjaM.jpg",
+            "http://i.imgur.com/76Jfv9b.jpg",
+            "http://i.imgur.com/fUX7EIB.jpg",
+            "http://i.imgur.com/syELajx.jpg",
+            "http://i.imgur.com/COzBnru.jpg",
+            "http://i.imgur.com/Z3QjilA.jpg",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +47,11 @@ public class PicassoActivity extends HttpActivity {
         initTopBar();
         setContentView(root);
 
-        Picasso.get()
-                .load("http://ww3.sinaimg.cn/large/610dc034jw1fasakfvqe1j20u00mhgn2.jpg")
-                .memoryPolicy(MemoryPolicy.NO_CACHE).into(mImageView);
+        //Picasso.get().setIndicatorsEnabled(true);
+//        Picasso.get().load("http://47.90.58.48:8080/ccicjpimptweb_file/lhs/20160323/5287%20(4).JPG")
+//                .memoryPolicy(MemoryPolicy.NO_CACHE).into(mImageView);
+
+        mListView.setAdapter(new PicassoListAdapter(this,imageUrls));
     }
     private void initTopBar() {
 
---
+++ app/src/main/java/com/zzz/myapp/adpater/PicassoListAdapter.java
@@ -0,0 +1,47 @@
+package com.zzz.myapp.adpater;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.zzz.myapp.R;
+
+public class PicassoListAdapter extends ArrayAdapter {
+    private Context mContext;
+
+    private String[] mImageUrls;
+
+    public PicassoListAdapter(Context context, String[] imageUrls) {
+        super(context, R.layout.picasso_adapter,imageUrls);
+        this.mContext = context;
+        this.mImageUrls = imageUrls;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView==null){
+            convertView = View.inflate(mContext,R.layout.picasso_adapter,null);
+        }
+        ImageView imageView = (ImageView)convertView;
+        if (TextUtils.isEmpty(mImageUrls[position])){
+            Picasso.get().cancelRequest(imageView);
+            imageView.setImageDrawable(null);
+        }else{
+            Picasso
+                    .get()
+                    .load(mImageUrls[position])
+                    .placeholder(R.mipmap.ic_launcher)
+                    .error(R.mipmap.ic_launcher)
+                    .centerCrop()
+                    .into((ImageView) convertView);
+        }
+        //加载图片
+//        Picasso.get().load(mImageUrls[position]).into((ImageView) convertView);
+
+        return convertView;
+    }
+}
